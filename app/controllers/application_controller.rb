8d227c5d6e00b127488afc9983224cc804f5b5c9
Dhanabal <dhanabal@vinu-Vostro-3560.(none)>
2013-12-27 14:38:21 +0530

    mlpush menu removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   def authenticate_user!
   	unless user_signed_in?
       redirect_to new_user_session_path,:notice=>"You are not a valid user"
+    end
+  end
+
+  def layout_by_resource
+  	if devise_controller? && resource_name == :user && action_name == 'new'
+      "login"
+    else
+      "application"
     end
   end
 end
